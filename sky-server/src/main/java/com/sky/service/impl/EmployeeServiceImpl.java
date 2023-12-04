1084921f5d1879118a78b5fe491f00378df80024
adminn123 <18210854820@163.com>
2023-12-04 09:20:47 +0800

    苍穹外卖提交

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -9,10 +9,13 @@ import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
+import java.nio.charset.StandardCharsets;
+@Slf4j
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
@@ -39,8 +42,11 @@ public class EmployeeServiceImpl implements EmployeeService {
         }
 
         //密码比对
-        // TODO 后期需要进行md5加密，然后再进行比对
-        if (!password.equals(employee.getPassword())) {
+        //
+//        DigestUtils.md5Hex(employee.getPassword());
+        String s = DigestUtils.md5DigestAsHex(password.getBytes(StandardCharsets.UTF_8));
+        log.info("获取的加密的密码为{}",s);
+        if (!s.equals(employee.getPassword())) {
             //密码错误
             throw new PasswordErrorException(MessageConstant.PASSWORD_ERROR);
         }
